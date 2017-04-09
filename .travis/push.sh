6beee15c55f9877bac6c32dc2b2f71b26815d17a
pstickne <pstick158@gmail.com>
2017-04-09 02:09:26 -0400

    removing extra output

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,12 +2,7 @@ git config --local user.name $GH_USER_NAME
 git config --local user.email $GH_USER_EMAIL
 
 git remote set-url origin https://$GITHUB_API_KEY@github.com/pstickne/Echo.git
-
-git status
-git branch -vv
 git checkout development
-git status
-git branch -vv
 
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
 
