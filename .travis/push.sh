3229df3043334fab1ba8d739fe7046ab224debfd
pstickne <pstick158@gmail.com>
2017-04-09 01:56:06 -0400

    attempt to fix the detached HEAD issue

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,11 +1,16 @@
 git config --local user.name $GH_USER_NAME
 git config --local user.email $GH_USER_EMAIL
 
+git remote set-url origin https://$GITHUB_API_KEY@github.com/pstickne/Echo.git
+
 git status
+git branch -vv
+git checkout development
+git status
+git branch -vv
 
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
 
-git remote set-url origin https://$GITHUB_API_KEY@github.com/pstickne/Echo.git
 git add .
 git commit -m "automated version bump [skip ci]"
 git push origin development
