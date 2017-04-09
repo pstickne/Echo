5d92a3d95c3f7685b485741e181e845ba4ce1289
pstickne <pstick158@gmail.com>
2017-04-09 01:24:29 -0400

    added version bump logic

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git config --local user.name "TravisCI"
-git config --local user.email "pstick158@gmail.com"
+git config --local user.name $GH_USER_NAME
+git config --local user.email $GH_USER_EMAIL
 
-printenv
+git status
+
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
+
+git remote set-url origin https://$GITHUB_API_KEY@github.com/pstickne/Echo.git
+git add .
+git commit -m "automated version bump [skip ci]"
+git push origin development
